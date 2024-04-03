49f820d7471e57e0fc0802c5c66d1e98d3f69417
NeilFlores21 <neijumar.manansala.flores@gmail.com>
2024-04-04 01:20:01 +0800

    Final Submission'

--- _middleware/authorize.js
+++ _middleware/authorize.js
@@ -8,7 +8,7 @@ function authorize(roles = []) {
     // roles param can be a single role string (e.g. Role. User or 'User')
     // or an array of roles (e.g. [Role.Admin, Role. User] or ['Admin', 'User'])
     if (typeof roles === 'string') {
-    roles = [roles];
+        roles = [roles];
     }
 
     return [
@@ -19,9 +19,9 @@ function authorize(roles = []) {
     async (req, res, next) => {
         const account = await db.Account.findByPk(req.user.id);
 
-        if (!account || (roles.length && !roles.includes (account.role))) {
+        if (!account || (roles.length && !roles.includes(account.role))) {
             // account no longer exists or role not authorized
-            return res.status(401).json({message: 'Unauthorized' });
+            return res.status(401).json({ message: 'Unauthorized' });
         }
 
         // authentication and authorization successful
--- accounts/refresh-token.model.js
+++ accounts/refresh-token.model.js
@@ -4,28 +4,27 @@ module.exports = model;
 
 function model(sequelize) {
     const attributes = {
-        token: { type: DataTypes.STRING},
-        expires: { type: DataTypes.DATE},
-        created: { type: DataTypes. DATE, allowNull: false, defaultValue: DataTypes.NOW },
-        createdByIp: { type: DataTypes.STRING},
-        revoked: { type: DataTypes.DATE},
-        revokedByIp: { type: DataTypes.STRING},
-        replacedByToken: {type: DataTypes. STRING},
+        token: { type: DataTypes.STRING },
+        expires: { type: DataTypes.DATE },
+        created: { type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW },
+        createdByIp: { type: DataTypes.STRING },
+        revoked: { type: DataTypes.DATE },
+        revokedByIp: { type: DataTypes.STRING },
+        replacedByToken: { type: DataTypes.STRING },
         isExpired: {
             type: DataTypes.VIRTUAL,
             get() { return Date.now() >= this.expires; }
         },
-            isActive: {
+        isActive: {
             type: DataTypes.VIRTUAL,
-            get() { return this.revoked && !this.isExpired; }
+            get() { return !this.revoked && !this.isExpired; } // Modified condition
         }
     };
 
-const options = {
+    const options = {
+        // Disable default timestamp fields (createdAt and updatedAt)
+        timestamps: false
+    };
 
-// disable default timestamp fields (createdAt and updatedAt)
-timestamps: false
-};
-
-return sequelize.define('refreshToken', attributes, options);
+    return sequelize.define('refreshToken', attributes, options);
 }
